e4681f7f3b8e3a3d05df88112a6fa2e90eb507ab
akseart <akseart@ya.ru>
2018-06-01 22:56:13 +0300

    Initial Commit

---
+++ prektika/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prektika
+//
+//  Created by Артем on 01.06.2018.
+//  Copyright © 2018 Артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
